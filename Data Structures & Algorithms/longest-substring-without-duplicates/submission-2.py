89bdab11cb13e22766fad85f932f319a35718e4e
AeroApollo <130645713+AeroApollo@users.noreply.github.com>
2026-08-22 16:51:16 -0500

    Add: longest-substring-without-duplicates - submission-2

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-2.py
@@ -0,0 +1,48 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        
+        #traverse the list in a for loop for c in s:
+        # given an empty list sub = [] and longest = 0
+        # if c is not in s: sub.append(c) 
+        # else: if len(sub) > longest: longest = len(sub) and sub =[c]
+
+        #time O(N) and space is O(number of characters in list)
+
+        #current method doesn't take into account substring that start with a later unique char
+
+        # if we encounter a duplicate char:
+        # 1) we check if the current sub is the longest and update if needed
+        # 2) remove all chars in sub up to the first instance of duplicate
+
+        '''
+        sub = []; longest = 0
+        for c in s:
+            if c not in sub: sub.append(c)
+            else: #c is a duplicate
+                #print(sub)
+                if len(sub) > longest: #check if current sub is longest 
+                    longest = len(sub)
+
+                first_ind = sub.index(c) # index of first instance of duplicate
+                sub = sub[first_ind+1:]
+                sub.append(c)
+                #print(sub)
+                
+
+        # last sub build was uncheck so check
+        if len(sub) > longest:
+            return len(sub)
+        return longest
+        '''
+        # sliding window
+        sub = set()
+        left = 0
+        longest = 0
+        for right in range(len(s)):
+            while s[right] in sub: #this while acts like an if
+                sub.remove(s[left]) #keep rm letters from the left until duplicate is rm
+                left += 1 #after removing left, we don't need to see it again
+            sub.add(s[right]) # if s[r] not in set then add it
+            longest = max(longest,right-left+1) #+1 bc 0 ind
+        return longest
